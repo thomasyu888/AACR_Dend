d373f9e5f9e04017f556d9619c43b5d927cc936c
Thomas Yu <thomasyu888@gmail.com>
2015-05-27 11:04:20 -0700

    Change width

--- server.R
+++ server.R
@@ -19,6 +19,6 @@ colnames(Annotations) <- "Title"
 
 shinyServer(function(input, output) {
   output$myChart <- renderIHeatmap(
-    iHeatmap(t(M),colAnnote = Annotations,ClustM ="ward.D2",Rowv = FALSE, showHeat = FALSE)
+    iHeatmap(t(M),colAnnote = Annotations,ClustM ="ward.D2",Rowv = FALSE, showHeat = FALSE,width=600)
   )
 })
