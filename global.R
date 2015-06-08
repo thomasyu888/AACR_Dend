376e7646bbc8fce93904c2e843fbda61c01b84e8
Thomas Yu <thomasyu888@gmail.com>
2015-06-08 15:54:46 -0700

    Add MAI

--- global.R
+++ global.R
@@ -5,18 +5,21 @@ library(synapseClient)
 synapseLogin()
 dat <- synGet('syn4260756')
 annot <- synGet("syn4260757")
+tem <- synGet("syn4382386")
 m<- read.delim(dat@filePath,sep="\t")
 d<- read.delim(annot@filePath,sep="\t")
+fix <- read.table(tem@filePath,sep="\t")
 
 h<- hclust(dist(m),method="ward.D2")
 cut <- cutree(h,k=20)
+
 annotation <- unlist(lapply(unique(cut), function(x) {
-  cluster <- fixed[which(cut[h$order]==x),]
+  cluster <- fix[which(cut[h$order]==x),]
   allCone <- unique(unlist(apply(cluster, 1, function(j){
     lapply(j, function(y) strsplit(y,"|",fixed=TRUE)[[1]][1])
   })))
-  dog <- unlist(cluster)
-  dog <- unlist(lapply(dog, function(d) strsplit(d,"|",fixed=TRUE)[[1]][1]))
+  dog <- as.matrix(unlist(cluster))
+  dog <- unlist(lapply(dog, function(d) strsplit(d,"|")[[1]][1]))
   dog <- gsub(" ","",dog)
   foo <- gsub(" ","",allCone)
   temp <- unlist(lapply(foo, function(z) length(which(z==as.matrix(dog)))))
