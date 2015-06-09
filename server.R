c6fc2b8396c61a2496304d7207481e9e8962e13b
Thomas Yu <thomasyu888@gmail.com>
2015-06-08 18:42:22 -0700

    addOnInfo

--- server.R
+++ server.R
@@ -1,7 +1,9 @@
 library(synapseClient)
 shinyServer(function(input, output) {
   output$myChart <- renderIHeatmap({
-    iHeatmap(t(m),Addon = d,colAnnote = newcut, ClustM ="ward.D2",Rowv = FALSE,showHeat = FALSE, width=600)
+    iHeatmap(t(m),addOnInfo = d,colAnnote = newcut, ClustM ="ward.D2",Rowv = FALSE,showHeat = FALSE, width=600)
     })
 })
+
+
 
